39414740bdab9f35c2181500f6ce38328d1ae971
shaikhsirin <shaikhsirin8080example.com>
2022-03-07 09:00:34 +0530

    Gambling

---
+++ src/Gambler.java
@@ -0,0 +1,9 @@
+public class Gambler{
+    public static void main(String[] args){
+        final int Evrey_Day_Stake = 100;
+        int stake = 100;
+        int bet = 1;
+System.out.println("Welocome Gambling simulation");
+    }
+}
+
